11c2f850243ce8acb3686126b3277885c363c7ba
quyet <hole.in1.NE19007>
2021-02-10 10:35:21 +0900

    asdf

---
+++ asdf.py
@@ -0,0 +1,12 @@
+# Atcoder Beginner's Selection Practice ABC081A
+'''
+s=input()
+tokens =s.split()
+a=int(tokens[0])
+b=int(tokens[1])
+'''
+a,b=map(int,input().split())
+if a*b%2==0:
+    print('Even')
+else:
+    print('Odd')
